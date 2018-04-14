87d26342c154e22dd48f2785e7382ba2892b7be2
angrykir <angrykir@gmail.com>
2018-04-14 18:53:09 +0300

    3 commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  handleClickPlus() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+  handleClickMinus() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +28,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <button onClick={this.handleClickPlus.bind(this)}>Plus</button>
+          <button onClick={this.handleClickMinus.bind(this)}>Minus</button>
+          <div>{this.state.count}</div>
         </p>
       </div>
     );
